25be30b3850392f7b1f588dce042d96c35ebf439
Jacob Michelsen <chromegadget@gmail.com>
2025-10-29 21:53:01 +0100

    Wallpaper update

--- modules/styling/stylix.nix
+++ modules/styling/stylix.nix
@@ -17,7 +17,6 @@ in
 
   config = lib.mkIf cfg.enable {
     stylix.enable = true;
-    #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-    stylix.image = ../../homeManagerModules/styles/current-wallpaper.png;
+    stylix.image = ../../homeManagerModules/styles/current-wallpaper.jpg;
   };
 }
